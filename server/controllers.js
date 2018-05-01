21b9521e350e335644a3b12d75ab7973f7fcb9cd
iayt <ayiltek@gmail.com>
2018-05-02 01:24:09 +0300

    update

--- server/controllers.js
+++ server/controllers.js
@@ -1,3 +1,25 @@
 	exports.home = (req, res) => {
-		 res.send('<a href="javascript:;">Google İle Giriş Yap</a>');
+		 res.send('<a href="/auth/google">Google İle Giriş Yap</a>');
+	};
+
+
+	exports.login = (req, res) => {
+		res.send('Girş Yapınız...');
+
+		//let email = req.body.email;
+		//let password = req.body.password;
+
+	};
+
+
+	// gmail.users.messages.list - method => https://github.com/google/google-api-nodejs-client/blob/master/src/apis/gmail/v1.ts#L2137
+	// gmail.users.messages.list - sample => https://github.com/google/google-api-nodejs-client/blob/master/samples/gmail/list.js
+	exports.mails_list = (req, res) => {
+		 res.send('Mailler Listelendi...');
+	};
+
+
+	// gmail.users.mesages.get - method => https://github.com/google/google-api-nodejs-client/blob/master/src/apis/gmail/v1.ts#L1990
+	exports.mail_read = (req, res) => {
+		 res.send(req.params.mail_id + ' id\'li mail açıldı...');
 	};
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,101 @@
-module.exports = (app) => {
+const request = require('request');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+const {google} = require('googleapis');
+const gmail = google.gmail('v1');
+const auth = new google.auth.OAuth2;
+const dotenv = require('dotenv').config();
+/* const mongoose = require('mongoose'); */
+
+
+
+passport.serializeUser(function(user, done) {
+  done(null, user); // done(null, user.id);
+});
+
+passport.deserializeUser(function(obj, done) {
+  done(null, obj); // Users.findById(obj, done);
+});
+
+
+
+passport.use(new GoogleStrategy(
+  {
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+
+  	/*
+	  	console.log(profile.id);
+	  	console.log(profile.displayName);
+	*/
+
+  	// Kullanıcının ID'si
+  	// Error Kontrolü
+
+	auth.setCredentials({
+		access_token: accessToken,
+		refresh_token: refreshToken
+	});
+
+
+
+	return done(null, profile); 
+  	
+  	/*
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+	*/
+  }
+));
+
+
+// https://expressjs.com/en/guide/routing.html
+module.exports = (app) => { 
 
 	const controllers = require('./controllers.js');
 
-	app.route('/').get(controllers.home);
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+ 
+	app.route('/')
+		.get(controllers.home);
+		//.post(controllers.login);
+
+	app.get('/mails',
+		function(req, res) {
+			console.log('burası /mails sayfası...');
+
+			gmail.users.messages.list({ auth:auth, userId:'me', maxResults:5 },
+			function(err, response) {
+				console.log(response.data);
+			});
+
+		}
+	);
+
+	app.route('/mail/:mail_id')
+		.get(controllers.mail_read);
+		//.put(controllers.mail_update)
+		//.delete(controllers.mail_delete)
+		//.post(controllers.mail_create);
+
+ 
+
+	app.get('/auth/google',
+		passport.authenticate('google', { scope: ['profile','https://www.googleapis.com/auth/gmail.readonly'] }));
+
+	app.get('/auth/google/callback', 
+		passport.authenticate('google', { failureRedirect: '/login' }),
+		function(req, res) {	
+			res.redirect('/mails');
+		}
+	);
 
 };
